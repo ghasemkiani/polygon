be597d14862bb2c7423f69332e271ce55c09754a
Ghasem Kiani <ghasemkiani@gmail.com>
2022-11-09 23:44:21 +0330

    update

--- ichain.js
+++ ichain.js
@@ -2,12 +2,14 @@ import {util} from "./util.js";
 import {Util} from "./util.js";
 import {Account} from "./account.js";
 import {Contract} from "./contract.js";
+import {Token} from "./token.js";
 
 const ichain = {
 	util,
 	Util,
 	Account,
 	Contract,
+	Token,
 };
 
 export {ichain};
